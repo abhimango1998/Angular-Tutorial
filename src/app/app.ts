c1be8d4a05ca6b5a207c21a0031147909dacf789
abhimango1998 <abhishek.kumar@mangoitsolutions.in>
2025-10-10 19:12:57 +0530

    till lecture 44

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header';
+import { User } from './user/user';
+import { DUMMY_USERS } from './dummyUsers';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, User, Tasks],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get getSelectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectedUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
+
+interface IUser {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: IUser;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return '/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
